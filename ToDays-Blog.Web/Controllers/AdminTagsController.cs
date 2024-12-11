00874a482b7a0e65091d7eedb73ce7bf83efe77e
dayross <dianarocio.czapata@gmail.com>
2024-12-11 15:20:40 -0600

    added 'add' view

---
+++ ToDays-Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDays_Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Add2()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Edit2()
+        {
+            return View();
+        }
+    }
+}
